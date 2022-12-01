01a018a12ef080665db7fc0cee7e2451206b6a07
Stelin Raj A <stelinraj4@gmail.com>
2022-12-01 22:58:48 +0530

    react.js

--- src/FormInput.js
+++ src/FormInput.js
@@ -1,22 +1,24 @@
 import './App.css'
 
-function FormInput({handleChange, formInputData, handleSubmit}){
+function FormInput({handleChange, formInputData, handleSubmit, show, Update}){
     return(
         <div className="form">
           <div className="row">
-            <label for="fullName">Fullname</label>
+            <label htmlFor="fullName">Fullname:</label>
             <input type="text" onChange={handleChange} value={formInputData.fullName} name="fullName" className="form-control"  placeholder="Full Name" />
           </div>
           <div className="row">
-          <label for="emailAddress">Email</label>
+          <label htmlFor="emailAddress">Email:</label>
             <input type="email" onChange={handleChange} value={formInputData.emailAddress} name="emailAddress" className="form-control" placeholder="Email Address" />
-            git push -u origin main   </div>
-          <div className="row">
-          <label for="password">Password</label>
-            <input type="text" onChange={handleChange} value={formInputData.salary} name="password" className="form-control" placeholder="Password" />
           </div>
           <div className="row">
-            <input type="submit" onClick={handleSubmit} className="btn btn-primary" />
+          <label htmlFor="password">Password:</label>
+            <input type="text" onChange={handleChange} value={formInputData.password} name="password" className="form-control" placeholder="Password" />
+          </div>
+          <div className="row">
+
+           { !show ? <input type="button" value="Submit" onClick={handleSubmit} className="btn btn-primary" />:
+           <input type="button" value="Update" onClick={Update} className="btn btn-success" />}
           </div>
         </div>
      
